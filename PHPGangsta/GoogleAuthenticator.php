2758e69177b13227061af6b295c9388fef33cad2
Neto <tynhomello@gmail.com>
2022-11-14 19:05:19 -0300

    Removed hardcoded code length checking

--- PHPGangsta/GoogleAuthenticator.php
+++ PHPGangsta/GoogleAuthenticator.php
@@ -126,7 +126,7 @@ class PHPGangsta_GoogleAuthenticator
             $currentTimeSlice = floor(time() / 30);
         }
 
-        if (strlen($code) != 6) {
+        if (strlen($code) !== $this->_codeLength) {
             return false;
         }
 
